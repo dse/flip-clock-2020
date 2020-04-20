73dd64d1743d121e5141e4c8d0a76a70a2bd48e4
Darren Embry <dse@webonastick.com>
2020-04-20 18:44:43 -0400

    objectify

--- flipclock-page.js
+++ flipclock-page.js
@@ -1,146 +1,174 @@
-var fakeItalic = false;
-var fontStyle = 'normal';
-
-function main () {
-    setPropertiesFromStorage();
-    setFormValues();
-    addEvents();
-}
-
-function addEvents() {
-    document.addEventListener('change', function (event) {
-        if (event.target.hasAttribute('data-flipclock-segment-foreground-color')) {
-            document.documentElement.style.setProperty('--segment-foreground-color', event.target.value);
-            localStorage.setItem('flipclock--segment-foreground-color', event.target.value);
+var FlipClockPage = {
+    fakeItalic: false,
+    fontStyle: 'normal',
+    init: function () {
+        this.setPropertiesFromStorage();
+        this.setFormValues();
+        this.addEvents();
+    },
+    addEvents: function () {
+        document.addEventListener('click', function (event) {
+            var element = event.target.closest('.segment');
+            if (!element) {
+                return;
+            }
+            var id = element.getAttribute('data-segment-id');
+            if (!id) {
+                return;
+            }
+            var segment = Segment.segmentsById[id];
+            if (!segment) {
+                return;
+            }
+            segment.flipWrap();
+        }.bind(this));
+        document.addEventListener('change', function (event) {
+            var style = document.documentElement.style;
+            if (event.target.hasAttribute('data-flipclock-segment-foreground-color')) {
+                style.setProperty('--segment-foreground-color', event.target.value);
+                localStorage.setItem('flipclock--segment-foreground-color', event.target.value);
+            }
+            if (event.target.hasAttribute('data-flipclock-segment-background-color')) {
+                style.setProperty('--segment-background-color', event.target.value);
+                localStorage.setItem('flipclock--segment-background-color', event.target.value);
+            }
+            if (event.target.hasAttribute('data-flipclock-background-color')) {
+                style.setProperty('--background-color', event.target.value);
+                localStorage.setItem('flipclock--background-color', event.target.value);
+            }
+            if (event.target.hasAttribute('data-flipclock-font-family')) {
+                var value = event.target.options[event.target.selectedIndex].value;
+                this.fakeItalic = event.target.options[event.target.selectedIndex].hasAttribute('data-fake-italic');
+                this.addOrRemoveFakeItalicClass();
+                style.setProperty('--font-family', value);
+                localStorage.setItem('flipclock--font-family', value);
+            }
+            if (event.target.hasAttribute('data-flipclock-font-weight')) {
+                var value = event.target.options[event.target.selectedIndex].value;
+                style.setProperty('--font-weight', value);
+                localStorage.setItem('flipclock--font-weight', value);
+            }
+            if (event.target.hasAttribute('data-flipclock-font-style')) {
+                var value = event.target.options[event.target.selectedIndex].value;
+                style.setProperty('--font-style', value);
+                localStorage.setItem('flipclock--font-style', value);
+                this.fontStyle = value;
+                this.addOrRemoveItalicClass();
+            }
+        }.bind(this));
+    },
+    setPropertiesFromStorage: function () {
+        var style = document.documentElement.style;
+        if (localStorage.getItem('flipclock--segment-foreground-color')) {
+            style.setProperty('--segment-foreground-color', localStorage.getItem('flipclock--segment-foreground-color'));
         }
-        if (event.target.hasAttribute('data-flipclock-segment-background-color')) {
-            document.documentElement.style.setProperty('--segment-background-color', event.target.value);
-            localStorage.setItem('flipclock--segment-background-color', event.target.value);
+        if (localStorage.getItem('flipclock--segment-background-color')) {
+            style.setProperty('--segment-background-color', localStorage.getItem('flipclock--segment-background-color'));
         }
-        if (event.target.hasAttribute('data-flipclock-background-color')) {
-            document.documentElement.style.setProperty('--background-color', event.target.value);
-            localStorage.setItem('flipclock--background-color', event.target.value);
+        if (localStorage.getItem('flipclock--background-color')) {
+            style.setProperty('--background-color', localStorage.getItem('flipclock--background-color'));
         }
-        if (event.target.hasAttribute('data-flipclock-font-family')) {
-            var value = event.target.options[event.target.selectedIndex].value;
-            fakeItalic = event.target.options[event.target.selectedIndex].hasAttribute('data-fake-italic');
-            addOrRemoveFakeItalicClass();
-            document.documentElement.style.setProperty('--font-family', value);
-            localStorage.setItem('flipclock--font-family', value);
+        if (localStorage.getItem('flipclock--font-family')) {
+            style.setProperty('--font-family', localStorage.getItem('flipclock--font-family'));
         }
-        if (event.target.hasAttribute('data-flipclock-font-weight')) {
-            var value = event.target.options[event.target.selectedIndex].value;
-            document.documentElement.style.setProperty('--font-weight', value);
-            localStorage.setItem('flipclock--font-weight', value);
+        if (localStorage.getItem('flipclock--font-weight')) {
+            style.setProperty('--font-weight', localStorage.getItem('flipclock--font-weight'));
         }
-        if (event.target.hasAttribute('data-flipclock-font-style')) {
-            var value = event.target.options[event.target.selectedIndex].value;
-            document.documentElement.style.setProperty('--font-style', value);
-            localStorage.setItem('flipclock--font-style', value);
-            fontStyle = value;
-            addOrRemoveItalicClass();
+        if (localStorage.getItem('flipclock--font-style')) {
+            style.setProperty('--font-style', localStorage.getItem('flipclock--font-style'));
         }
-    });
-}
-
-function setPropertiesFromStorage() {
-    if (localStorage.getItem('flipclock--segment-foreground-color')) {
-        document.documentElement.style.setProperty('--segment-foreground-color', localStorage.getItem('flipclock--segment-foreground-color'));
-    }
-    if (localStorage.getItem('flipclock--segment-background-color')) {
-        document.documentElement.style.setProperty('--segment-background-color', localStorage.getItem('flipclock--segment-background-color'));
-    }
-    if (localStorage.getItem('flipclock--background-color')) {
-        document.documentElement.style.setProperty('--background-color', localStorage.getItem('flipclock--background-color'));
-    }
-    if (localStorage.getItem('flipclock--font-family')) {
-        document.documentElement.style.setProperty('--font-family', localStorage.getItem('flipclock--font-family'));
-    }
-    if (localStorage.getItem('flipclock--font-weight')) {
-        document.documentElement.style.setProperty('--font-weight', localStorage.getItem('flipclock--font-weight'));
-    }
-    if (localStorage.getItem('flipclock--font-style')) {
-        document.documentElement.style.setProperty('--font-style', localStorage.getItem('flipclock--font-style'));
-    }
-}
-
-function setFormValues() {
-    var cs  = getComputedStyle(document.documentElement);
-    Array.from(document.querySelectorAll('[data-flipclock-segment-foreground-color]')).forEach(function (input) {
-        var value = cs.getPropertyValue('--segment-foreground-color').trim();
+    },
+    setFormValues: function () {
+        var cs  = getComputedStyle(document.documentElement);
+        Array.from(document.querySelectorAll('[data-flipclock-segment-foreground-color]')).forEach(function (input) {
+            var value = cs.getPropertyValue('--segment-foreground-color').trim();
+            this.setInputValue(input, value);
+        }.bind(this));
+        Array.from(document.querySelectorAll('[data-flipclock-segment-background-color]')).forEach(function (input) {
+            var value = cs.getPropertyValue('--segment-background-color').trim();
+            this.setInputValue(input, value);
+        }.bind(this));
+        Array.from(document.querySelectorAll('[data-flipclock-background-color]')).forEach(function (input) {
+            var value = cs.getPropertyValue('--background-color').trim();
+            this.setInputValue(input, value);
+        }.bind(this));
+        Array.from(document.querySelectorAll('[data-flipclock-font-family]')).forEach(function (input) {
+            var value = cs.getPropertyValue('--font-family').trim();
+            var option;
+            if (input.tagName.toLowerCase() === 'select') {
+                option = this.setSelectValue(input, value);
+                this.fakeItalic = option.hasAttribute('data-fake-italic');
+                this.addOrRemoveFakeItalicClass();
+            } else {
+                this.setInputValue(input, value);
+            }
+        }.bind(this));
+        Array.from(document.querySelectorAll('[data-flipclock-font-weight]')).forEach(function (input) {
+            var value = cs.getPropertyValue('--font-weight').trim();
+            if (input.tagName.toLowerCase() === 'select') {
+                this.setSelectValue(input, value);
+            } else {
+                this.setInputValue(input, value);
+            }
+        }.bind(this));
+        Array.from(document.querySelectorAll('[data-flipclock-font-style]')).forEach(function (input) {
+            var value = cs.getPropertyValue('--font-style').trim();
+            if (input.tagName.toLowerCase() === 'select') {
+                this.setSelectValue(input, value);
+                this.fontStyle = value;
+                this.addOrRemoveItalicClass();
+            } else {
+                this.setInputValue(input, value);
+            }
+        }.bind(this));
+    },
+    setInputValue: function (input, value) {
+        if (input.tagName.toLowerCase() === 'select') {
+            return this.setSelectValue(input, value);
+        }
         input.value = value;
-    });
-    Array.from(document.querySelectorAll('[data-flipclock-segment-background-color]')).forEach(function (input) {
-        var value = cs.getPropertyValue('--segment-background-color').trim();
-        input.value = value;
-    });
-    Array.from(document.querySelectorAll('[data-flipclock-background-color]')).forEach(function (input) {
-        var value = cs.getPropertyValue('--background-color').trim();
-        input.value = value;
-    });
-    Array.from(document.querySelectorAll('[data-flipclock-font-family]')).forEach(function (input) {
-        var value = cs.getPropertyValue('--font-family').trim();
+        return input;
+    },
+    setSelectValue: function (input, value) {
+        var options;
         var option;
-        if (input.tagName.toLowerCase() === 'select') {
-            option = setSelectValue(input, value);
-            fakeItalic = option.hasAttribute('data-fake-italic');
-            addOrRemoveFakeItalicClass();
+        options = Array.from(input.options).filter(function (option) {
+            return option.value === value;
+        });
+        if (options.length) {
+            input.selectedIndex = options[0].index;
+            return options[0];
         } else {
-            input.value = value;
+            option = document.createElement('option');
+            option.setAttribute('value', value);
+            option.appendChild(document.createTextNode(value));/* label */
+            input.add(option);
+            input.selectedIndex = option.index;
+            return option;
         }
-    });
-    Array.from(document.querySelectorAll('[data-flipclock-font-weight]')).forEach(function (input) {
-        var value = cs.getPropertyValue('--font-weight').trim();
-        if (input.tagName.toLowerCase() === 'select') {
-            setSelectValue(input, value);
-        } else {
-            input.value = value;
-        }
-    });
-    Array.from(document.querySelectorAll('[data-flipclock-font-style]')).forEach(function (input) {
-        var value = cs.getPropertyValue('--font-style').trim();
-        if (input.tagName.toLowerCase() === 'select') {
-            setSelectValue(input, value);
-            fontStyle = value;
-            addOrRemoveItalicClass();
-        } else {
-            input.value = value;
-        }
-    });
-}
-
-function setSelectValue(input, value) {
-    var options;
-    var option;
-    options = Array.from(input.options).filter(function (option) {
-        return option.value === value;
-    });
-    if (options.length) {
-        input.selectedIndex = options[0].index;
-        return options[0];
-    } else {
-        option = document.createElement('option');
-        option.setAttribute('value', value);
-        option.appendChild(document.createTextNode(value));/* label */
-        input.add(option);
-        input.selectedIndex = option.index;
-        return option;
+    },
+    addOrRemoveFakeItalicClass: function () {
+        Array.from(document.querySelectorAll('.flip-clock')).forEach(function (element) {
+            element.classList[this.fakeItalic    ? 'add' : 'remove']('with-fake-italic');
+            element.classList[(!this.fakeItalic) ? 'add' : 'remove']('without-fake-italic');
+        }.bind(this));
+    },
+    addOrRemoveItalicClass: function () {
+        Array.from(document.querySelectorAll('.flip-clock')).forEach(function (element) {
+            element.classList[this.fontStyle === 'normal'  ? 'add' : 'remove']('with-font-style--normal');
+            element.classList[this.fontStyle === 'italic'  ? 'add' : 'remove']('with-font-style--italic');
+            element.classList[this.fontStyle === 'oblique' ? 'add' : 'remove']('with-font-style--oblique');
+        }.bind(this));
+    },
+    setDefaults: function () {
+        var style = document.documentElement.style;
+        style.removeProperty('--segment-foreground-color');
+        style.removeProperty('--segment-background-color');
+        style.removeProperty('--background-color');
+        style.removeProperty('--font-family');
+        style.removeProperty('--font-weight');
+        style.removeProperty('--font-style');
     }
-}
+};
 
-function addOrRemoveFakeItalicClass() {
-    Array.from(document.querySelectorAll('.flip-clock')).forEach(function (element) {
-        element.classList[fakeItalic    ? 'add' : 'remove']('with-fake-italic');
-        element.classList[(!fakeItalic) ? 'add' : 'remove']('without-fake-italic');
-    });
-}
-
-function addOrRemoveItalicClass() {
-    Array.from(document.querySelectorAll('.flip-clock')).forEach(function (element) {
-        element.classList[fontStyle === 'normal'  ? 'add' : 'remove']('with-font-style--normal');
-        element.classList[fontStyle === 'italic'  ? 'add' : 'remove']('with-font-style--italic');
-        element.classList[fontStyle === 'oblique' ? 'add' : 'remove']('with-font-style--oblique');
-    });
-}
-
-main();
+FlipClockPage.init();
