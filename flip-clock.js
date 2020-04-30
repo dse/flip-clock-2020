9fe3e9274f5ad7a0b41936a94949777585320478
Darren Embry <dse@webonastick.com>
2020-04-30 02:38:24 -0400

    finally, safari audio has no delays

--- flip-clock.js
+++ flip-clock.js
@@ -31,18 +31,56 @@ function absoluteURL(url) {
     return a.href;
 }
 
-var FlipClockAudio = (function () {
-    function FlipClockAudio(url) {
-        this.url = url = absoluteURL(url);
-        this.audio = new Audio(url);
-        this.audio.volume = 1;
-    }
-    FlipClockAudio.prototype.play = function () {
-        this.audio.currentTime = 0;
-        this.audio.play();
-    };
-    return FlipClockAudio;
-}());
+// polyfill
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+
+var FlipClockAudio;
+
+if (window.AudioContext && (location.protocol === 'http:' || location.protocol === 'https:')) {
+    FlipClockAudio = (function () {
+        var context = new AudioContext();
+        var audioCollection = {};
+        function FlipClockAudio(url) {
+            this.audio = audioCollection[url];
+            if (!this.audio) {
+                this.audio = audioCollection[url] = {};
+                var request = new XMLHttpRequest();
+                request.open('GET', url, true);
+                request.responseType = 'arraybuffer';
+                request.onload = function () {
+                    context.decodeAudioData(request.response, function (buffer) {
+                        this.audio.buffer = buffer;
+                    }.bind(this));
+                }.bind(this);
+                request.send();
+            }
+        }
+        FlipClockAudio.prototype.play = function () {
+            if (!this.audio.buffer) {
+                return;
+            }
+            var source = context.createBufferSource();
+            source.buffer = this.audio.buffer;
+            source.connect(context.destination);
+            source.start(0);
+        };
+        return FlipClockAudio;
+    }());
+} else {
+    // fallback that works well in Chrome but not Safari.
+    FlipClockAudio = (function () {
+        function FlipClockAudio(url) {
+            this.url = url = absoluteURL(url);
+            this.audio = new Audio(url);
+            this.audio.volume = 1;
+        }
+        FlipClockAudio.prototype.play = function () {
+            this.audio.currentTime = 0;
+            this.audio.play();
+        };
+        return FlipClockAudio;
+    }());
+}
 
 /**
  * requestAnimationFrame polyfill
