837e175fb48a6537e5a8859b59f8a066305f057b
Darren Embry <dse@webonastick.com>
2020-12-08 12:45:49 -0500

    move FlipClockAudio class to own file

--- js/flip-clock.js
+++ js/flip-clock.js
@@ -150,54 +150,6 @@ function absoluteURL(url) {
 // polyfill
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 
-var FlipClockAudio;
-
-if (window.AudioContext && (location.protocol === 'http:' || location.protocol === 'https:')) {
-    FlipClockAudio = (function () {
-        var context = new AudioContext();
-        var audioCollection = {};
-        function FlipClockAudio(url) {
-            this.audio = audioCollection[url];
-            if (!this.audio) {
-                this.audio = audioCollection[url] = {};
-                var request = new XMLHttpRequest();
-                request.open('GET', url, true);
-                request.responseType = 'arraybuffer';
-                request.onload = function () {
-                    context.decodeAudioData(request.response, function (buffer) {
-                        this.audio.buffer = buffer;
-                    }.bind(this));
-                }.bind(this);
-                request.send();
-            }
-        }
-        FlipClockAudio.prototype.play = function () {
-            if (!this.audio.buffer) {
-                return;
-            }
-            var source = context.createBufferSource();
-            source.buffer = this.audio.buffer;
-            source.connect(context.destination);
-            source.start(0);
-        };
-        return FlipClockAudio;
-    }());
-} else {
-    // fallback that works well in Chrome but not Safari.
-    FlipClockAudio = (function () {
-        function FlipClockAudio(url) {
-            this.url = url = absoluteURL(url);
-            this.audio = new Audio(url);
-            this.audio.volume = 1;
-        }
-        FlipClockAudio.prototype.play = function () {
-            this.audio.currentTime = 0;
-            this.audio.play();
-        };
-        return FlipClockAudio;
-    }());
-}
-
 // stackoverflow says absolute URLs work in safari
 var tickURL = absoluteURL('sounds/tick2.wav');
 
---
+++ js/main.js
@@ -0,0 +1,31 @@
+function loadScriptSync(url, options) {
+    "use strict";
+    var xhr = new window.XMLHttpRequest();
+    xhr.open('GET', url, Boolean(options && options.async));
+    if (!(options && options.noAcceptHeader)) {
+        xhr.setRequestHeader('Accept', 'text/javascript, application/javascript, application/ecmascript, application/x-ecmascript, */*; q=0.01');
+    }
+    if (!(options && options.crossDomain)) {
+        xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest'); // not for cross-domain
+    }
+    var callback = options && options.onload;
+    xhr.onload = function () {
+        if (!(xhr.status === 0 || xhr.status === 200)) {
+            return;
+        }
+        var script = document.createElement('script');
+        script.text = xhr.responseText;
+        document.head.appendChild(script).parentNode.removeChild(script);
+        if (callback) {
+            callback();
+        }
+    };
+    xhr.send(null);
+}
+
+var cacheBuster = window.hasCacheBuster ? ('?cacheBuster=' + new Date().getTime()) : '';
+
+loadScriptSync('js/jfont-checker.js' + cacheBuster);
+loadScriptSync('js/audio-ticker.js' + cacheBuster);
+loadScriptSync('js/flip-clock.js' + cacheBuster);
+loadScriptSync('js/flip-clock-page.js' + cacheBuster);
