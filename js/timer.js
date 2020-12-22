d1d39080eaba1ce2b37a27d86fe301e9d03f287b
Darren Embry <dse@webonastick.com>
2020-12-21 21:36:53 -0500

    cleanup

--- js/timer.js
+++ js/timer.js
@@ -1,20 +1,11 @@
 var TimeTicker = (function () {
-    function TimeTicker(options) {
-        this.testRollover = options && options.testRollover;
+    function TimeTicker() {
     }
     TimeTicker.prototype.start = function () {
-        var date = new Date();
-        if (this.testRollover) {
-            this.msOffset = this.testRollover.getTime() - 20000 - date.getTime();
-            delete this.testRollover;
-        }
-        if (this.msOffset) {
-            date.setTime(date.getTime() + this.msOffset);
-        }
-        this.callback(date);
+        this.callback();
         this.timeout = setTimeout(function () {
             this.start();
-        }.bind(this), 1000 - date.getTime() % 1000);
+        }.bind(this), 1000 - new Date().getTime() % 1000);
     };
     TimeTicker.prototype.stop = function () {
         if (this.timeout) {
